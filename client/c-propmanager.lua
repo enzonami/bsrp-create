c6df2b9549e44c064cc8b3f97c305a70e6f738e5
enzonami <160043231+enzonami@users.noreply.github.com>
2024-12-17 03:56:50 -0600

    Update c-propmanager.lua

--- client/c-propmanager.lua
+++ client/c-propmanager.lua
@@ -1,72 +1,24 @@
-
 -- 𝕭𝖑𝖆𝖈𝖐 𝕾𝖍𝖆𝖉𝖊𝖘 Prop-Manager
 -- This module spawns a list of predefined props / models at predefined locations.
+
 local PropManager = {}
 
 local Config = {
     Props = { -- List of props to spawn
-
-    --Example Locations
-        {
-            Model = "xm_prop_x17_chest_closed", --https://gta-objects.xyz/objects
-            Position = vector3(1559.15, 6651.86, 1.61), --Location
-            Heading = 0.0 --Rotation
-        },
-        {
-            Model = "xm_prop_x17_chest_closed", --https://gta-objects.xyz/objects
-            Position = vector3(1795.24, 6583.31, 53.48), --Location
-            Heading = 0.0 --Rotation
-        },
-        {
-            Model = "xm_prop_x17_chest_closed", --https://gta-objects.xyz/objects
-            Position = vector3(2157.97, 6371.01, 184.29), --Location
-            Heading = 0.0 --Rotation
-        },
-        {
-            Model = "xm_prop_x17_chest_closed", --https://gta-objects.xyz/objects
-            Position = vector3(2751.84, 6543.07, 0.72), --Location
-            Heading = 0.0 --Rotation
-        },
-        {
-            Model = "xm_prop_x17_chest_closed", --https://gta-objects.xyz/objects
-            Position = vector3(2828.03, 5979.24, 347.15), --Location
-            Heading = 0.0 --Rotation
-        },
-        {
-            Model = "xm_prop_x17_chest_closed", --https://gta-objects.xyz/objects
-            Position = vector3(2909.53, 5474.84, 184.65), --Location
-            Heading = 0.0 --Rotation
-        },
-        {
-            Model = "xm_prop_x17_chest_closed", --https://gta-objects.xyz/objects
-            Position = vector3(3444.38, 5175.65, 3.76), --Location
-            Heading = 0.0 --Rotation
-        },
-        {
-            Model = "xm_prop_x17_chest_closed", --https://gta-objects.xyz/objects
-            Position = vector3(3612.79, 5024.97, 10.35), --Location
-            Heading = 0.0 --Rotation
-        },
-        {
-            Model = "xm_prop_x17_chest_closed", --https://gta-objects.xyz/objects
-            Position = vector3(3197.62, 4602.26, 174.79), --Location
-            Heading = 0.0 --Rotation
-        },
-        {
-            Model = "xm_prop_x17_chest_closed", --https://gta-objects.xyz/objects
-            Position = vector3(3937.93, 4387.37, 15.49), --Location
-            Heading = 0.0 --Rotation
-        },
-        {
-            Model = "xm_prop_x17_chest_closed", --https://gta-objects.xyz/objects
-            Position = vector3(3287.04, 3140.41, 252.53), --Location
-            Heading = 0.0 --Rotation
-        },
-        {
-            Model = "xm_prop_x17_chest_closed", --https://gta-objects.xyz/objects
-            Position = vector3(3464.03, 2592.33, 18.34), --Location
-            Heading = 0.0 --Rotation
-        }
+        -- Example Locations
+        {Model = "xm_prop_x17_chest_closed", Position = vector3(1559.15, 6651.86, 1.61), Heading = 0.0},
+        {Model = "xm_prop_x17_chest_closed", Position = vector3(1795.24, 6583.31, 53.48), Heading = 0.0},
+        {Model = "xm_prop_x17_chest_closed", Position = vector3(2157.97, 6371.01, 184.29), Heading = 0.0},
+        {Model = "xm_prop_x17_chest_closed", Position = vector3(2751.84, 6543.07, 0.72), Heading = 0.0},
+        {Model = "xm_prop_x17_chest_closed", Position = vector3(2828.03, 5979.24, 347.15), Heading = 0.0},
+        {Model = "xm_prop_x17_chest_closed", Position = vector3(2909.53, 5474.84, 184.65), Heading = 0.0},
+        {Model = "xm_prop_x17_chest_closed", Position = vector3(3444.38, 5175.65, 3.76), Heading = 0.0},
+        {Model = "xm_prop_x17_chest_closed", Position = vector3(3612.79, 5024.97, 10.35), Heading = 0.0},
+        {Model = "xm_prop_x17_chest_closed", Position = vector3(3197.62, 4602.26, 174.79), Heading = 0.0},
+        {Model = "xm_prop_x17_chest_closed", Position = vector3(3937.93, 4387.37, 15.49), Heading = 0.0},
+        {Model = "xm_prop_x17_chest_closed", Position = vector3(3287.04, 3140.41, 252.53), Heading = 0.0},
+        {Model = "xm_prop_x17_chest_closed", Position = vector3(3464.03, 2592.33, 18.34), Heading = 0.0}
+        --https://gta-objects.xyz/objects
     }
 }
 
@@ -107,7 +59,7 @@ function PropManager.spawnProp(model, position, heading)
     table.insert(SpawnedProps, prop)
 end
 
--- Function to cleanup all spawned props
+-- Function to clean up all spawned props
 function PropManager.cleanupProps()
     for _, prop in ipairs(SpawnedProps) do
         if DoesEntityExist(prop) then
@@ -119,6 +71,7 @@ end
 
 -- Initialize Props
 function PropManager.initialize()
+    PropManager.cleanupProps()
     for _, prop in ipairs(Config.Props) do
         PropManager.spawnProp(prop.Model, prop.Position, prop.Heading)
     end
@@ -131,10 +84,12 @@ AddEventHandler('onResourceStop', function(resourceName)
     end
 end)
 
--- Automatically initialize on resource start
+-- Automatically initialize on resource start with a delay
 AddEventHandler('onResourceStart', function(resourceName)
     if GetCurrentResourceName() == resourceName then
-        PropManager.initialize()
+        Citizen.SetTimeout(1000, function() -- 1-second delay to ensure dependencies are loaded
+            PropManager.initialize()
+        end)
     end
 end)
 
